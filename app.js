4eb9c64abd4b436585a0169d58e8c09f8bb7925a
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-02-28 17:03:42 -0300

    Finished the project!

--- app.js
+++ app.js
@@ -36,6 +36,8 @@ function win (userChoice, computerChoice) {
     // result_p.innerHTML = convertToWord(userChoice) + " beats " + convertToWord(computerChoice)  + ". You win!"
     // BELOW IT'S ANOTHER WAY OF WRITING IT
     result_p.innerHTML = ` ${convertToWord(userChoice)} ${smallUserWord}  beats  ${convertToWord(computerChoice)} ${smallCompWord} . You win!`
+    document.getElementById(userChoice).classList.add('green-glow');
+    setTimeout( () => document.getElementById(userChoice).classList.remove('green-glow'), 500)
 }
 
 function lose (userChoice, computerChoice) {
@@ -47,6 +49,8 @@ function lose (userChoice, computerChoice) {
     // result_p.innerHTML = convertToWord(userChoice) + " beats " + convertToWord(computerChoice)  + ". You win!"
     // BELOW IT'S ANOTHER WAY OF WRITING IT
     result_p.innerHTML = ` ${convertToWord(userChoice)} ${smallUserWord} loses to ${convertToWord(computerChoice)} ${smallCompWord} . You lost!`
+    document.getElementById(userChoice).classList.add('red-glow');
+    setTimeout( () =>  document.getElementById(userChoice).classList.remove('red-glow'), 500);
 }
 
 function draw (userChoice, computerChoice) {
@@ -55,6 +59,8 @@ function draw (userChoice, computerChoice) {
     // result_p.innerHTML = convertToWord(userChoice) + " beats " + convertToWord(computerChoice)  + ". You win!"
     // BELOW IT'S ANOTHER WAY OF WRITING IT
     result_p.innerHTML = ` ${convertToWord(userChoice)} ${smallUserWord}  equals ${convertToWord(computerChoice)} ${smallCompWord} . It's a Draw!`
+    document.getElementById(userChoice).classList.add('grey-glow');
+    setTimeout( () => document.getElementById(userChoice).classList.remove('grey-glow'), 500)
 }
 
 
